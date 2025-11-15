be5a732e7f5aa755fb30829fb4a8cfca3731975e
ManuelGraziosi <manuel.graziosi@gmail.com>
2025-11-15 18:46:45 +0100

    feat: filter by genre (wip)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
-import movies from "./assets/movies"
+import { useState } from "react"
+import moviesArray from "./assets/movies"
 
 function App() {
+  const [movies, setMovies] = useState(moviesArray)
+  const [filteredMovies, setFilteredMovies] = useState(movies)
+
+  const genreArray = [];
+  moviesArray.forEach((curMovie) => {
+    if (!genreArray.includes(curMovie.genre)) {
+      genreArray.push(curMovie.genre)
+    }
+  });
+
+  console.log("genreArray", genreArray)
 
   return (
     <>
@@ -12,21 +24,34 @@ function App() {
         <main className="mt-4">
           {/* Fileter Section */}
           <section>
-            <select name="movieGenre" id="movieGenre">
-              <option value="">--Please choose an option--</option>
-              <option value="Genere1">Genere1</option>
+            <select name="movieGenre" id="movieGenre" onChange={(event) => console.log(event.target.value)}>
+              <option value="">--scegli un'opzione--</option>
+              {
+                genreArray.map((curGenre, index) => (
+                  <option key={index} value={curGenre}>{curGenre}</option>
+                ))
+              }
             </select>
           </section>
 
           {/* Movies Catalog Section*/}
           <section className="mt-4">
-            <ul>
+            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3">
               {
-                movies.map((curMovie) => (
-                  <li>{curMovie.title}</li>
+                filteredMovies.map((curMovie, index) => (
+                  <div key={index} className="col g-3">
+                    <div className="card h-100">
+                      <div className="card-header">
+                        <h5 className="card-title">{curMovie.title}</h5>
+                      </div>
+                      <div className="card-body">
+                        <p className="card-text">Genere: {curMovie.genre}</p>
+                      </div>
+                    </div>
+                  </div>
                 ))
               }
-            </ul>
+            </div>
           </section>
         </main >
       </div>

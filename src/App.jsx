b98ea7bdfd6595a33e60dfadaa4d08fd840956b5
ManuelGraziosi <manuel.graziosi@gmail.com>
2025-11-15 17:53:55 +0100

    feat: base structure completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
+import movies from "./assets/movies"
 
 function App() {
 
   return (
     <>
-      <h2>React-Movie-Filter</h2>
+      <div className="container">
+        <header className="mt-4">
+          <h2>React-Movie-Filter</h2>
+        </header>
+
+        <main className="mt-4">
+          {/* Fileter Section */}
+          <section>
+            <select name="movieGenre" id="movieGenre">
+              <option value="">--Please choose an option--</option>
+              <option value="Genere1">Genere1</option>
+            </select>
+          </section>
+
+          {/* Movies Catalog Section*/}
+          <section className="mt-4">
+            <ul>
+              {
+                movies.map((curMovie) => (
+                  <li>{curMovie.title}</li>
+                ))
+              }
+            </ul>
+          </section>
+        </main >
+      </div>
     </>
   )
 }
